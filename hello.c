6066a38e702e3aaca41d777c77fddbea08a09ea7
heechanhan <66683520+heechanhan@users.noreply.github.com>
2020-06-09 09:14:45 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Hee-Chan Han
+ *
+ * Date: 06/09/2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hee-Chan Han\n");
+
+  return 0;
+}
